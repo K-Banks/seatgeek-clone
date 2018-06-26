34d2014fabc4c478450dddc374c4850a4648d27f
Bobby Martin <robert.martin.619@gmail.com>
2018-06-25 22:00:29 -0700

    Add eslint and styled-jsx

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function Ticket(props){
   return (
-      <div>
-        <h3>{props.location} - {props.names}</h3>
-        <p><em>{props.issue}</em></p>
-        <hr/>
-      </div>
-   );
+    <div>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 Ticket.propTypes = {
