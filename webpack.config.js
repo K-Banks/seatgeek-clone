42c8a2117636f8a0c01eb53530c244aea777f4dc
Bobby Martin <robert.martin.619@gmail.com>
2018-06-26 09:37:29 -0700

    add style and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,19 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
         test: /\.jsx?$/,
         enforce: "pre",
         loader: "eslint-loader",
