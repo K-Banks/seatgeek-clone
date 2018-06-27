c84b2398db0a5a1add7bf40123688896c163c0d4
Bobby Martin and Kayl Eubanks <kayl@>
2018-06-27 14:31:19 -0700

    Add styles to event items

--- src/components/Body/Events/EventLocationFilter/EventLocationFilter.jsx
+++ src/components/Body/Events/EventLocationFilter/EventLocationFilter.jsx
@@ -3,8 +3,19 @@ import styles from './EventLocationFilter.css';
 
 function EventLocationFilter() {
   return(
-    <div>
-
+    <div className={styles.wrapper}>
+      <div  className={styles.styler}>
+        <span>
+          <svg class="Icon__Svg-s1hyhuxm-0 kVIzRj" fill="#494949" stroke="none" color="#494949" height="1em" width="1em" viewBox="0 0 24 24"><path d="M12,13 C10.343,13 9,11.657 9,10 C9,8.343 10.343,7 12,7 C13.657,7 15,8.343 15,10 C15,11.657 13.657,13 12,13 M12,4 C8.686,4 6,6.686 6,10 C6,15 12,20 12,20 C12,20 18,15.063 18,10 C18,6.686 15.314,4 12,4"></path></svg>
+          Portland,OR
+        </span>
+      </div>
+      <div>
+        <span>
+          Any Date
+          <svg width="7px" height="4px" viewBox="0 0 7 4" class="sg-dropdown__arrow"><g transform="translate(-77, -9)" fill="#7A7A7A"><path d="M80.50025,13 L77.14625,9.81121873 C76.95125,9.62582447 76.95125,9.32539069 77.14625,9.13904569 C77.34225,8.95365144 77.65825,8.95365144 77.85425,9.13904569 L80.50025,11.6556539 L83.14625,9.13904569 C83.34225,8.95365144 83.65825,8.95365144 83.85425,9.13904569 C84.04925,9.32539069 84.04925,9.62582447 83.85425,9.81121873 L80.50025,13 Z"></path></g></svg>
+        </span>
+      </div>
     </div>
   );
 }
--- src/components/Body/Events/EventSection/EventSection.jsx
+++ src/components/Body/Events/EventSection/EventSection.jsx
@@ -5,7 +5,12 @@ import styles from './EventSection.css';
 function EventSection() {
   return(
     <div>
-
+      <div className={styles.eventSection}>
+        <EventItem />
+        <div className={styles.showMore}>
+          <svg width="16px" height="12px" viewBox="0 0 16 12" version="1.1"><g transform="translate(-17, -19) translate(25, 83) scale(-1, 1) translate(-25, -83)" fill="#1673E6"><path d="M23.6999998,28.8999999 L23.6999998,18.3000002 C23.6999998,17.7000002 24.0999998,17.3000002 24.6999998,17.3000002 C25.2999998,17.3000002 25.6999998,17.7000002 25.6999998,18.3000002 L25.6999998,28.8999999 L28.9999998,25.6 C29.3999998,25.2 29.9999998,25.2 30.3999998,25.6 C30.7999998,26 30.7999998,26.6 30.3999998,27 L24.6999998,32.6999998 L18.9999998,27 C18.5999998,26.6 18.5999998,26 18.9999998,25.6 C19.3999998,25.2 19.9999998,25.2 20.3999998,25.6 L23.6999998,28.8999999 Z" transform="translate(24.700000, 25) rotate(90) translate(-24.700000, -25)"></path></g></svg>
+        </div>
+      </div>
     </div>
   );
 }
